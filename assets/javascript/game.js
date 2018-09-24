5c8e4fa5e43ed729989c73e3b8f4ad9d84ab34ee
sahenke13 <shenke13@gmail.com>
2018-09-23 22:57:59 -0500

    HW4V2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,76 +1,140 @@
 window.onload=function(){
     
-    
-    //create 4 characters    
-    var arya={
-        attackPower: 10,
-        counterAttack: 60,
-        hp: 200
-    };
-    var jonSnow={
-        attackPower: 15,
-        counterAttack: 10,
-        hp: 150
-    };
-    var nightKing={
-        attackPower: 25,
-        counterAttack: 40,
-        hp: 200
-    };
-    var theHound={
-        attackPower: 20,
-        counterAttack: 20,
-        hp: 160
-    };
-
-   
     //declare global variables
 
     var selectedChar;
+    var selectedCharIndex;
     var selectedEnemy;
+    var enemyChar;
+    var enemyCharIndex;
     var charCount = 0;
-    var enenmyCount = 0;
+    var enemyCount = 0;
+    var defeated = 0;
+    var winCount = 0;
+    //create 4 characters    
+   var charBank=[ 
+        {
+        name: "arya",
+        attackPower: 20,
+        counterAttack: 60,
+        hp: 200
+    },
+        {
+        name: "jonSnow",
+        attackPower: 15,
+        counterAttack: 10,
+        hp: 120
+    },
+        {
+        name: "theHound",
+        attackPower: 20,
+        counterAttack: 30,
+        hp: 120
+    },
+        {
+        name: "NightKing",
+        attackPower: 25,
+        counterAttack: 40,
+        hp: 175
+    }
+    ]
+    
+  
+    
 
-    $(".char").on("click",function(){
-        //set selected char
-        selectedChar = $(this.id);
-        console.log(this.id);
-        console.log(selectedChar.counterAttack)
-        //append selected charb
-      if(charCount==0){
-        $("#urChar").append(selectedChar);
-        charCount = 1;
-      }else if(charCount ==1 && enenmyCount==0){
-        $("#enemies").append(selectedChar);
-        enenmyCount = 1;
-      }
-      console.log("attack power is: " + this.id);
+    //Click on char elements to select your Hero and your enemies
+    $(".char").on("click",function()
+    {
 
+    if(charCount === 0 && enemyCount ===0)
+      {
+        selectedCharIndex = parseInt($(this).attr("value"));
+        selectedChar = charBank[selectedCharIndex];
+        $('#charBankLabel').text("select which Enemy to attack");
+        console.log("Selected Character");
+        $(this).detach().appendTo("#urChar");  
+        charCount=1;
+        return;
+        }
+        if(charCount ===1 && enemyCount ===0)
+        {   
+            $('#charBankLabel').text("select an enemy to attack");
+            enemyCharIndex = parseInt($(this).attr("value"));
+            enemyChar = charBank[enemyCharIndex];
+            $(this).detach().appendTo("#enemyChar");
+            $('#charBankLabel').text("select an enemy to attack");
+            enemyCount = 1;  
+            return;
+        }  else if(charCount ===1 && enemyCount ===1)
+        {
+            alert("You need to attack your enemy")
+        } 
 
+    });
 
-    })
+        $("#attack").on("click",function(){
+            attack(enemyChar,selectedChar);
+            if(defeated = 1 ){
+                $("#char #urFoe").detach().appendTo("#vanquished")
 
+            }
 
+        });
+       
+      
+      
+        //function to attact x is enemy and y is your selected character
 
+        function attack(x, y){
+            console.log("The enemy is: " + x.name);
+            console.log("Your hero is: " + y.name);
+            
+    
+            if(x.hp >0 && y.hp >0)
+            {         
+                x.hp =  x.hp - y.attackPower;
+                y.hp = y.hp - x.counterAttack;
+                y.attackPower = y.attackPower + 15;
+    
+                console.log("enemyHeath is " + x.hp);
+                console.log("Hero Health is "+ y.hp);
+                if(y.hp<=0)
+                {
+                alert("Valer Morghulis")
 
+                } 
+                //if enemy dies
+                else if(x.hp<=0)
+                {
+                    console.log("You have vanquished your enemy")
+                    defeated = 1;
+                    enemyCount = 0;
+                    winCount++;
+                }
+            }
+            if(winCount==3){
+                alert("You have vanquished your enemies.")
+            }
+            return defeated, enemyCount, winCount;
+    
+        }
+    }
 
+       
+        
 
+           
+    
+        
+    
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
+    //     if(charCount==0)
+    //   {
+    //     console.log("test");
+    //     console.log(selectedChar);
+    //     console.log(charBank[1])
+            
+    //       for(let i = 0; i<charBank.length; i++){
+    //           console.log(i);
+    //       }
+    //     }
